543c8e35856222e5c13c5a43ef6490d17e2f9e6d
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-01-25 17:16:42 +0100

    card with parent

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ class Product {
     }
 
     public function getCard() {
+        
 
         return $this -> price
         . " <br> " . $this -> category 
@@ -30,13 +31,20 @@ class Toys extends Product{
     public $toys_name;
     
 
-    public function __construct(, $price, $category, $type, $toys_name ) {
+    public function __construct( $price, $category, $type, $toys_name ) {
 
-        /* $this -> price = $price;
+        $this -> price = $price;
         $this -> category = $category;
-        $this->type = $type; */
+        $this->type = $type;
         $this -> toys_name = $toys_name;
       
+    }
+
+    public function getName(){
+
+        return $this->getCard() . " <br> " . $this->toys_name . " <br> <br> <br> ";
+
+
     }
 
 
@@ -55,13 +63,33 @@ class Food extends Product{
 
     public function __construct( $price, $category, $type, $food_name ) {
 
-     /* $this -> price = $price;
+        $this -> price = $price;
         $this -> category = $category;
-        $this->type = $type; */
+        $this->type = $type;
         $this -> food_name = $food_name;
         
+    }
+
+    public function getName(){
+
+        return $this->getCard() . " <br> " . $this->food_name . " <br> <br> <br> ";
     }
 
 
 }
 
+$ball = new Toys('7.50 £', 'cat', 'toys', 'magic ball');
+$kibble = new Food('9.50 £', 'dog', 'food', 'gnammy');
+$bone = new Toys('12 £', 'dog', 'toys', 'amazing bone');
+$catFood = new Food('21 £', 'cat', 'food', 'deluxe');
+
+
+echo '<h1>';
+
+echo $ball -> getName();
+echo $kibble -> getName();
+echo $bone -> getName();
+echo $catFood -> getName();
+
+
+echo "</h1>";
